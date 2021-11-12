65e382e9b7d93c1cc626f413e8f59804790f9721
DamonCharlesRoberts <damon.roberts-1@colorado.edu>
2021-11-11 17:21:30 -0700

    mar,mnar,mcar efficiency boost

--- code/cleaning_simulating.py
+++ code/cleaning_simulating.py
@@ -6,71 +6,20 @@
 	#* Updated by: dcr
 # Setup
 	#* Load modules
-import wget
-wget.download('https://raw.githubusercontent.com/BorisMuzellec/MissingDataOT/master/utils.py')
 import numpy as np #Numpy for arrays
 import pandas as pd #Pandas for DataFrames
-import torch #For produce_NA function
-import matplotlib.pyplot as plt
-import seaborn as sns #For heatmap
-from utils import *
 
-#import random as rand # Dropping random values in array
-	#* Define Ampute Function called produce_NA
-def produce_NA(X, p_miss, mecha="MCAR", opt=None, p_obs=None, q=None):
-    """
-    Generate missing values for specifics missing-data mechanism and proportion of missing values. 
-    
-    Parameters
-    ----------
-    X : torch.DoubleTensor or np.ndarray, shape (n, d)
-        Data for which missing values will be simulated.
-        If a numpy array is provided, it will be converted to a pytorch tensor.
-    p_miss : float
-        Proportion of missing values to generate for variables which will have missing values.
-    mecha : str, 
-            Indicates the missing-data mechanism to be used. "MCAR" by default, "MAR", "MNAR" or "MNARsmask"
-    opt: str, 
-         For mecha = "MNAR", it indicates how the missing-data mechanism is generated: using a logistic regression ("logistic"), quantile censorship ("quantile") or logistic regression for generating a self-masked MNAR mechanism ("selfmasked").
-    p_obs : float
-            If mecha = "MAR", or mecha = "MNAR" with opt = "logistic" or "quanti", proportion of variables with *no* missing values that will be used for the logistic masking model.
-    q : float
-        If mecha = "MNAR" and opt = "quanti", quantile level at which the cuts should occur.
-    
-    Returns
-    ----------
-    A dictionnary containing:
-    'X_init': the initial data matrix.
-    'X_incomp': the data with the generated missing values.
-    'mask': a matrix indexing the generated missing values.s
-    """
-    
-    to_torch = torch.is_tensor(X) ## output a pytorch tensor, or a numpy array
-    if not to_torch:
-        X = X.astype(np.float32)
-        X = torch.from_numpy(X)
-    if mecha == "MAR":
-        mask = MAR_mask(X, p_miss, p_obs).double()
-    elif mecha == "MNAR" and opt == "logistic":
-        mask = MNAR_mask_logistic(X, p_miss, p_obs).double()
-    elif mecha == "MNAR" and opt == "quantile":
-        mask = MNAR_mask_quantiles(X, p_miss, q, 1-p_obs).double()
-    elif mecha == "MNAR" and opt == "selfmasked":
-        mask = MNAR_self_mask_logistic(X, p_miss).double()
-    else:
-        mask = (torch.rand(X.shape) < p_miss).double()
-    
-    X_nas = X.clone()
-    X_nas[mask.bool()] = np.nan
-    
-    return {'X_init': X.double(), 'X_incomp': X_nas.double(), 'mask': mask}
+    #* Source produce_na_funciton.py script for produce_NA fxn
+exec(open('code/produce_na_function.py').read())
+
+
 
 # Generate simulated data
 	#* Set Seed
 np.random.seed(601)
-	#* Two variables
-mean1 = np.random.random(2) # Randomly generate the means for 2 variables
-cov1 = np.random.random((2,2)) # Randomly generate the covariances for 2 variables
+	#* Three variables
+mean1 = np.random.random(3) # Randomly generate the means for 2 variables
+cov1 = np.random.random((3,3)) # Randomly generate the covariances for 3 variables
 sim1 = np.random.multivariate_normal(mean1, cov1, 1000) #Use mean and covariates to generate a simulated dataset following a multivariate normal distribution with an N = 1000
 #sim1 = pd.DataFrame(sim1, columns = ['Column_A', ['Column_B']]) # Convert this simulated dataset into a dataframe and give it column names
 	#* MCAR
@@ -109,13 +58,13 @@ sim10_sparse_mnar = produce_NA(sim10, p_miss = 0.4, mecha = "MNAR", opt = "logis
 sim10_mnar = sim10_sparse_mnar['X_incomp']
 # Save Simulated and Cleaned data
 	#* Simulated
-sim1df = pd.DataFrame(sim1, columns = ['Column_A', 'Column_B'])
+sim1df = pd.DataFrame(sim1, columns = ['Column_A', 'Column_B', 'Column_C'])
 pd.DataFrame.to_csv(sim1df, path_or_buf='/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_binary.csv', index = True)
-sim1_mar = pd.DataFrame(sim1_mar, columns = ['Column_A', 'Column_B'])
+sim1_mar = pd.DataFrame(sim1_mar, columns = ['Column_A', 'Column_B', 'Column_C'])
 pd.DataFrame.to_csv(sim1_mar, path_or_buf='/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_binary_sparse_mar.csv', index = True)
-sim1_mcar = pd.DataFrame(sim1_mcar, columns = ['Column_A', 'Column_B'])
+sim1_mcar = pd.DataFrame(sim1_mcar, columns = ['Column_A', 'Column_B', 'Column_C'])
 pd.DataFrame.to_csv(sim1_mcar, path_or_buf='/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_binary_sparse_mcar.csv', index = True)
-sim1_mnar = pd.DataFrame(sim1_mnar, columns = ['Column_A', 'Column_B'])
+sim1_mnar = pd.DataFrame(sim1_mnar, columns = ['Column_A', 'Column_B', 'Column_C'])
 pd.DataFrame.to_csv(sim1_mnar, path_or_buf='/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_binary_sparse_mnar.csv', index = True)
 sim10 = pd.DataFrame(sim10, columns = ['Column_A', 'Column_B', 'Column_C', 'Column_D', 'Column_E', 'Column_F', 'Column_G', 'Column_H', 'Column_I', 'Column_J'])
 pd.DataFrame.to_csv(sim10, path_or_buf='/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_ndim.csv', index = True)
--- code/wvs_missingness.py
+++ code/wvs_missingness.py
@@ -6,66 +6,16 @@
     #* Updated by: dcr
 # Setup
     #* Load modules
-import wget
-wget.download('https://raw.githubusercontent.com/BorisMuzellec/MissingDataOT/master/utils.py')
 import numpy as np #Numpy for arrays
 import pandas as pd #Pandas for DataFrames
-import torch #For produce_NA function
-import matplotlib.pyplot as plt
-import seaborn as sns #For heatmap
-from utils import *
+
+
+    #* Source produce_na_funciton.py script for produce_NA fxn
+exec(open('/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/code/produce_na_function.py').read())
 
 #import random as rand # Dropping random values in array
     #* Load world values data
 wvs = pd.read_csv('/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_original.csv')
-    #* Define Ampute Function called produce_NA
-def produce_NA(X, p_miss, mecha="MCAR", opt=None, p_obs=None, q=None):
-    """
-    Generate missing values for specifics missing-data mechanism and proportion of missing values. 
-    
-    Parameters
-    ----------
-    X : torch.DoubleTensor or np.ndarray, shape (n, d)
-        Data for which missing values will be simulated.
-        If a numpy array is provided, it will be converted to a pytorch tensor.
-    p_miss : float
-        Proportion of missing values to generate for variables which will have missing values.
-    mecha : str, 
-            Indicates the missing-data mechanism to be used. "MCAR" by default, "MAR", "MNAR" or "MNARsmask"
-    opt: str, 
-         For mecha = "MNAR", it indicates how the missing-data mechanism is generated: using a logistic regression ("logistic"), quantile censorship ("quantile") or logistic regression for generating a self-masked MNAR mechanism ("selfmasked").
-    p_obs : float
-            If mecha = "MAR", or mecha = "MNAR" with opt = "logistic" or "quanti", proportion of variables with *no* missing values that will be used for the logistic masking model.
-    q : float
-        If mecha = "MNAR" and opt = "quanti", quantile level at which the cuts should occur.
-    
-    Returns
-    ----------
-    A dictionnary containing:
-    'X_init': the initial data matrix.
-    'X_incomp': the data with the generated missing values.
-    'mask': a matrix indexing the generated missing values.s
-    """
-    
-    to_torch = torch.is_tensor(X) ## output a pytorch tensor, or a numpy array
-    if not to_torch:
-        X = X.astype(np.float32)
-        X = torch.from_numpy(X)
-    if mecha == "MAR":
-        mask = MAR_mask(X, p_miss, p_obs).double()
-    elif mecha == "MNAR" and opt == "logistic":
-        mask = MNAR_mask_logistic(X, p_miss, p_obs).double()
-    elif mecha == "MNAR" and opt == "quantile":
-        mask = MNAR_mask_quantiles(X, p_miss, q, 1-p_obs).double()
-    elif mecha == "MNAR" and opt == "selfmasked":
-        mask = MNAR_self_mask_logistic(X, p_miss).double()
-    else:
-        mask = (torch.rand(X.shape) < p_miss).double()
-    
-    X_nas = X.clone()
-    X_nas[mask.bool()] = np.nan
-    
-    return {'X_init': X.double(), 'X_incomp': X_nas.double(), 'mask': mask}
 
 # Clean 
     #* Set Seed
@@ -74,16 +24,76 @@ np.random.seed(601)
 wvs = wvs.drop(columns = ['version', 'doi', 'B_COUNTRY_ALPHA', 'C_COW_ALPHA', 'LNGE_ISO', 'CPARTY' ,'Partyname', 'Partyabb', 'CPARTYABB'])
 wvs.dropna(axis = 0, how = 'any', inplace = True)
 pd.DataFrame.to_csv(wvs, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_complete_obs_only.csv', index = True)
-wvs = wvs.to_numpy()
-    #* MCAR
-wvs_sparse_mcar = produce_NA(wvs, p_miss = 0.4, mecha = "MCAR")
-wvs_mcar = wvs_sparse_mcar['X_incomp']
-np.savetxt('/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_mcar.csv', wvs_mcar, delimiter =',')
-    #* MAR
-wvs_sparse_mar = produce_NA(wvs, p_miss = 0.4, mecha = "MAR", p_obs = 0.5)
-wvs_mar = wvs_sparse_mar['X_incomp']
-np.savetxt('/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_mar.csv', wvs_mar, delimiter =',')
-    #* MNAR
-wvs_sparse_mnar = produce_NA(wvs, p_miss = 0.4, mecha = "MNAR", opt = 'logistic', p_obs = 0.5)
-wvs_mnar = wvs_sparse_mnar['X_incomp']
-np.savetxt('/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_mnar.csv', wvs_mnar, delimiter = ',')
+wvs_colnames= wvs.keys()
+wvs_numpy = wvs.to_numpy()
+
+
+class wvs_missingness:
+        def __init__(wvs_i, types):
+            if types == 'MCAR':
+                wvs_i = produce_NA(wvs_numpy, p_miss = 0.4, mecha = types)
+            elif types == 'MAR':
+                wvs_i = produce_NA(wvs_numpy, p_miss = 0.4, mecha = types, p_obs = 0.5)
+            elif types == 'MNAR':
+                wvs_i = produce_NA(wvs_numpy, p_miss = 0.4, mecha = types, p_obs = 0.5, opt = 'logistic')
+            else:
+                print('Invalid argument')
+            wvs_i = wvs_i['X_incomp']
+            wvs_i = pd.DataFrame(wvs_i.numpy(), columns = wvs_colnames)
+            pd.DataFrame.to_csv(wvs_i, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_' + types + '.csv', index = True) 
+wvs_missingness(types = 'MCAR')
+wvs_missingness(types = 'MAR')
+wvs_missingness(types = 'MNAR')
+    #* MCAR - All Variables
+#wvs_mcar = produce_NA(wvs_numpy, p_miss = 0.4, mecha = "MCAR")
+#wvs_mcar = wvs_mcar['X_incomp']
+#wvs_mcar = pd.DataFrame(wvs_mcar, columns = wvs_colnames)
+#pd.DataFrame.to_csv(wvs_mcar, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_mcar.csv', index = True)
+    #* MAR - All Variables 
+#wvs_mar = produce_NA(wvs_numpy, p_miss = 0.4, mecha = "MAR", p_obs = 0.5)
+#wvs_mar = wvs_mar['X_incomp']
+#wvs_mar = pd.DataFrame(wvs_mar, columns = wvs_colnmaes)
+#pd.DataFrame.to_csv(wvs_mar, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_mar.csv', index = True)
+    #* MNAR - All Variables
+#wvs_mnar = produce_NA(wvs_numpy, p_miss = 0.4, mecha = "MNAR", opt = 'logistic', p_obs = 0.5)
+#wvs_mnar = wvs_mnar['X_incomp']
+#wvs_mnar = pd.DataFrame(wvs_mnar, columns = wvs_colnames)
+#pd.DataFrame.to_csv(wvs_mcar, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_mnar.csv', index = True)
+
+    #* Make a k = 10 dataframe
+wvs_low_k = wvs[['Q222', 'Q289', 'Q112', 'Q118', 'Q234', 'Q94', 'Q95', 'Q96', 'Q97', 'Q98', 'Q99', 'Q100', 'Q101', 'Q102', 'Q103', 'Q104', 'Q105']]
+pd.DataFrame.to_csv(wvs_low_k, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_complete_obs_low_k.csv', index = True)
+wvs_low_k_colnames = wvs_low_k.keys()
+wvs_low_k_numpy = wvs_low_k.to_numpy()
+
+class wvs_low_k_missingness:
+        def __init__(wvs_low_k_i, types):
+            if types == 'MCAR':
+                wvs_low_k_i = produce_NA(wvs_low_k_numpy, p_miss = 0.4, mecha = types)
+            elif types == 'MAR':
+                wvs_low_k_i = produce_NA(wvs_low_k_numpy, p_miss = 0.4, mecha = types, p_obs = 0.5)
+            elif types == 'MNAR':
+                wvs_low_k_i = produce_NA(wvs_low_k_numpy, p_miss = 0.4, mecha = types, p_obs = 0.5, opt = 'logistic')
+            else:
+                print('Invalid argument')
+            wvs_low_k_i = wvs_low_k_i['X_incomp']
+            wvs_low_k_i = pd.DataFrame(wvs_low_k_i.numpy(), columns = wvs_low_k_colnames)
+            pd.DataFrame.to_csv(wvs_low_k_i, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_low_k_' + types + '.csv', index = True) 
+wvs_low_k_missingness(types = 'MCAR')
+wvs_low_k_missingness(types = 'MAR')
+wvs_low_k_missingness(types = 'MNAR')
+    #* MCAR - Low K Variables
+#wvs_low_k_mcar = produce_NA(wvs_low_k_numpy, p_miss = 0.4, mecha = 'MCAR')
+#wvs_low_k_mcar = wvs_low_k_mcar['X_incomp']
+#wvs_low_k_mcar = pd.DataFrame(wvs_low_k_mcar, columns = wvs_low_k_colnames)
+#pd.DataFrame.to_csv(wvs_low_k_mcar, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_low_k_mcar.csv', index = True)
+    #* MAR - Low K Variables
+#wvs_low_k_mar = produce_NA(wvs_low_k_numpy, p_miss = 0.4, mecha = 'MAR')
+#wvs_low_k_mar = wvs_low_k_mar['X_incomp']
+#wvs_low_k_mar = pd.DataFrame(wvs_low_k_mar, columns = wvs_low_k_colnames)
+#pd.DataFrame.to_csv(wvs_low_k_mar, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_low_k_mar.csv', index = True)
+    #* MNAR - Low K Variables
+#wvs_low_k_mnar = produce_NA(wvs_low_k_numpy, p_miss = 0.4, mecha = 'MNAR')
+#wvs_low_k_mnar = wvs_low_k_mnar['X_incomp']
+#wvs_low_k_mnar = pd.DataFrame(wvs_low_k_mnar, columns = wvs_low_k_colnames)
+#pd.DataFrame.to_csv(wvs_low_k_mnar, path_or_buf = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputationdata/wvs_low_k_mnar.csv', index = True)
