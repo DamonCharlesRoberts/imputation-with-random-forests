02180e6ded38daddeb4e4b7f39af805a28a3cda5
DamonCharlesRoberts <damon.roberts-1@colorado.edu>
2021-11-13 17:43:28 -0700

    wvs models

--- code/primary_analysis.py
+++ code/primary_analysis.py
@@ -21,9 +21,12 @@ sim_3_comp = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/curre
 sim_3_comp = sim_3_comp.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
 sim_10_comp = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_ten_complete.csv')
 sim_10_comp = sim_10_comp.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+wvs_comp = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_complete_obs_only.csv')
 	#* Import MAR data
 sim_3_mar = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_three_MAR.csv')
 sim_10_mar = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_ten_MAR.csv')
+wvs_mar = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_sparse_MAR.csv')
+wvs_low_k_mar = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/wvs_low_k.csv')
 	#* Import MCAR data
 #sim_3_mcar = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_three_MCAR.csv')
 #sim_10_mcar = pd.read_csv(filepath_or_buffer = '/Users/damonroberts/Dropbox/current_projects/dcr_rf_imputation/data/sim_ten_MCAR.csv')
@@ -175,4 +178,55 @@ accuracy_10_i = sim_10_comp['ColumnI'] - sim_10_mice_comp_10['ColumnI']
 accuracy_10_i = accuracy_10_i.mean()
 accuracy_10 = [('Column B', accuracy_10_b), ('Column C', accuracy_10_c), ('Column F', accuracy_10_f), ('Column G', accuracy_10_g), ('Column H', accuracy_10_h), ('Column I', accuracy_10_i)]
 print(tabulate(accuracy_10, headers, tablefmt = 'latex', floatfmt = '.3f'))
+
 # World Values Survey
+	#* Full
+wvs_mar = wvs_mar.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+
+wvs_kernel = mf.ImputationKernel(
+                                   wvs_mar,
+                                   datasets=10,
+                                   save_all_iterations=True,
+                                   random_state=601)
+wvs_kernel.mice(10)
+
+wvs_mice_comp_1 = wvs_kernel.complete_data(dataset=0, inplace=False)
+wvs_mice_comp_2 = wvs_kernel.complete_data(dataset=1, inplace=False)
+wvs_mice_comp_3 = wvs_kernel.complete_data(dataset=2, inplace=False)
+wvs_mice_comp_4 = wvs_kernel.complete_data(dataset=3, inplace=False)
+wvs_mice_comp_5 = wvs_kernel.complete_data(dataset=4, inplace=False)
+wvs_mice_comp_6 = wvs_kernel.complete_data(dataset=5, inplace=False)
+wvs_mice_comp_7 = wvs_kernel.complete_data(dataset=6, inplace=False)
+wvs_mice_comp_8 = wvs_kernel.complete_data(dataset=7, inplace=False)
+wvs_mice_comp_9 = wvs_kernel.complete_data(dataset=8, inplace=False)
+wvs_mice_comp_10 = wvs_kernel.complete_data(dataset=9, inplace=False)
+
+
+wvs_mar_drop = wvs_mar.dropna(axis = 0, how = 'any', inplace = False)
+wvs_mar_drop = wvs_mar_drop.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+
+	#* Subset
+
+wvs_low_k_mar = wvs_low_k_mar.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+
+wvs_low_k_kernel = mf.ImputationKernel(
+                                   wvs_low_k_mar,
+                                   datasets=10,
+                                   save_all_iterations=True,
+                                   random_state=601)
+wvs_low_k_kernel.mice(10)
+
+wvs_low_k_mice_comp_1 = wvs_low_k_kernel.complete_data(dataset=0, inplace=False)
+wvs_low_k_mice_comp_2 = wvs_low_k_kernel.complete_data(dataset=1, inplace=False)
+wvs_low_k_mice_comp_3 = wvs_low_k_kernel.complete_data(dataset=2, inplace=False)
+wvs_low_k_mice_comp_4 = wvs_low_k_kernel.complete_data(dataset=3, inplace=False)
+wvs_low_k_mice_comp_5 = wvs_low_k_kernel.complete_data(dataset=4, inplace=False)
+wvs_low_k_mice_comp_6 = wvs_low_k_kernel.complete_data(dataset=5, inplace=False)
+wvs_low_k_mice_comp_7 = wvs_low_k_kernel.complete_data(dataset=6, inplace=False)
+wvs_low_k_mice_comp_8 = wvs_low_k_kernel.complete_data(dataset=7, inplace=False)
+wvs_low_k_mice_comp_9 = wvs_low_k_kernel.complete_data(dataset=8, inplace=False)
+wvs_low_k_mice_comp_10 = wvs_low_k_kernel.complete_data(dataset=9, inplace=False)
+
+
+wvs_low_k_mar_drop = wvs_low_k_mar.dropna(axis = 0, how = 'any', inplace = False)
+wvs_low_k_mar_drop = wvs_low_k_mar_drop.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
